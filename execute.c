a9f1da7932e7d7147f2bc33e2d037bfca7ac57ff
RimJoudi <joudirim93@gmail.com>
2021-04-14 11:19:01 +0000

    execute file function

---
+++ execute.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+/**
+ * exec_cmd - execve the comands from line.
+ * @array: Command to execute.
+ * Return: 0.
+ */
+
+int exec_cmd(char **array)
+{
+	pid_t pid;
+	int status;
+
+	if (array == NULL || array[0] == NULL)
+	{
+		return (-1);
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("error");
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		execve(array[0], array, NULL);
+	}
+	else
+	{
+		wait(&status);
+	}
+	return (0);
+}
