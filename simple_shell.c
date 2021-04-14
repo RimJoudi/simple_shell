f2ddcc82b54cb23ffd5a0c1f4247aa5b74dba356
maroua199525 <2646@holbertonschool.com>
2021-04-14 12:30:54 +0100

    main function

---
+++ simple_shell.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+/**
+ *main - main function
+ *Return: Always(0)
+ */
+int main(void)
+{
+	int status = 1;
+	char *line = NULL, **argv;
+  /*signal(SIGINT, sig_handler);*/
+	signal(SIGINT, sig_handler);
+  	while (status)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+	  		prompt();
+		} 
+		line = _readline();
+		if (line[0] == '\n' )
+		{
+	  		free(line);
+	  		continue;
+		}
+		argv = parse_line(line, DELIMIT);
+	 /* if (_strcmp(argv[0], "exit") == 0)
+		{
+		break;
+		}
+	   if (_strcmp(argv[0], "env") == 0)
+		{
+		return (print_env());
+		}
+	blt = check_builtin(argv[0]);
+	if (blt == 1)
+	 {
+		return (fun_builtin(argv));
+	 }
+	else
+	{*/
+		path_command(argv);
+	}
+	free(line);
+	free_array(argv);  
+	return (0);
+}
+
