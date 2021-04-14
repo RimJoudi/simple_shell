a5199c1ea63419cef43f61a12be74f8f0be85582
maroua199525 <2646@holbertonschool.com>
2021-04-14 12:07:59 +0100

    getenv

---
+++ _getenv.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+*_getenv - Gets the value of an environment variable.
+*@var_name: The variable name.
+*Return: Returns the value of the environment variable varname
+*/
+
+char *_getenv(char *var_name)
+{
+	int i, j;
+	char *variable_value;
+
+	if (var_name == NULL)
+		return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (var_name[j] == environ[i][j])
+		{
+			while (var_name[j])
+			{
+				if (var_name[j] != environ[i][j])
+				{
+					break;
+				}
+				j++;
+			}
+			if (var_name[j] == '\0')
+			{
+				variable_value = (environ[i] + j + 1);
+				return (variable_value);
+			}
+		}
+	}
+	return (NULL);
+}
