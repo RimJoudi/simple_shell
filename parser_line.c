31e157c8446770b6ade7b12fd513b91a0d07c7fe
maroua199525 <2646@holbertonschool.com>
2021-04-14 12:20:37 +0100

    split parser

---
+++ parser_line.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+* parse_line - parses the input
+* @line: string
+*@delim: the delimet
+*Return: array of string
+*/
+char **parse_line(char *line, char *delim)
+{
+	char **array;
+	char *token;
+	int i = 0;
+
+	if (line == NULL)
+		return (NULL);
+	array = malloc(sizeof(char *) * SIZE);
+	if (array == NULL)
+	{
+		write(STDERR_FILENO, "error\n", 6);
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, delim);
+	while (token != NULL)
+	{
+		array[i++] = token;
+		token = strtok(NULL, delim);
+	}
+	array[i] = NULL;
+
+	return (array);
+}
