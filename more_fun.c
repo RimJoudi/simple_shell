47e4a561fbaed20be08091d6a4e2e243303d17e5
maroua199525 <2646@holbertonschool.com>
2021-04-14 12:23:26 +0100

    help function

---
+++ more_fun.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+* sig_handler - checks if Ctrl C is pressed
+* @signum: int
+*Return: void
+*/
+void sig_handler(int signum)
+{
+
+	if (signum == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n$ ", 3);
+	}
+	/*Return type of the handler function should be void*/
+}
+
+/**
+ * prompt - Display Shell Prompt
+ */
+void prompt(void)
+{
+	write(STDOUT_FILENO, "$ ", 2);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
